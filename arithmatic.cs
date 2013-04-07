37ad9196a01dd3bbf323d33d67cfee0707cdd389
arjun1459 <arjun1459@gmail.com>
2013-04-08 01:06:21 +0530

    opators arithmatic

---
+++ arithmatic.cs
@@ -0,0 +1,11 @@
+class arthoper
+{
+public static void Main(string[] arg)
+{
+int a=4,b=4;
+System.Console.WriteLine(++a * ++a * ++a);
+System.Console.WriteLine(--b + --b + --b);
+System.Console.WriteLine(a%b);
+System.Console.WriteLine(4/8);
+}
+}
